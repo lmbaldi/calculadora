c4f4fc81a29015c300d8dedf134cd8298948d5f9
lmbaldi <luizmarcelobaldi@hotmail.com>
2021-01-28 17:12:48 -0300

    implement class Display

--- src/br/com/calc/visao/Display.java
+++ src/br/com/calc/visao/Display.java
@@ -1,14 +1,26 @@
 package br.com.calc.visao;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
 	
+	private JLabel label;
+	
 	public Display() {
-		setBackground(Color.GREEN);
+		setBackground(new Color(46, 49, 50));
+		label = new JLabel("1525,07");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("courier", Font.PLAIN, 30));
+		
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+		
+		add(label);
 	}
 
 }
